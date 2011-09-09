f88cd8ae86dcead0fe2a5d3dd4fc7c25cdbc8f68
Elpizo Choi <elpizo@buzzlabs.com>
2011-09-08 17:12:11 -0700

    better naming for accounts methods; rm untestable account API; add reports actions test

--- lib/citygrid/api/ad_center/account.rb
+++ lib/citygrid/api/ad_center/account.rb
@@ -10,12 +10,14 @@ class CityGrid
             "/account/account/v2"
           end
 
-          def create options = {}
+          # Adds new customer account to existing user
+          # http://docs.prod.cs/display/citygridv2/Internal+-+Account#Internal-Account-AddNewCustomerAccounttoExistingUser
+          def import_to_cg options = {}
             token = extract_auth_token options
             handle_response post(
               "#{base_uri}/account/account/customer/v2/mutate",
               :body    => options.to_json,
-              :headers => {"authToken" => token}
+              :headers => merge_headers("authToken" => token)
             )
           end
         end
