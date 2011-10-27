14391b66d875c850409967cfa4a1fb2c79ee7ce2
Philip Lee <phil@buzzlabs.com>
2011-10-27 14:41:39 -0700

    fix import_to_cg

--- lib/citygrid/api/ad_center/account.rb
+++ lib/citygrid/api/ad_center/account.rb
@@ -5,11 +5,7 @@ class CityGrid
         # Adds new customer account to existing user
         # http://docs.prod.cs/display/citygridv2/Internal+-+Account#Internal-Account-AddNewCustomerAccounttoExistingUser
         def self.import_to_cg options = {}
-          token = extract_auth_token options
-          request_and_handle :post,
-            "#{base_uri}/account/v2/customer/mutate",
-            :body    => options.to_json,
-            :headers => merge_headers("authToken" => token)
+          mutate options
         end
       end
     end
