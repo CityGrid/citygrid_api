32851456c87128ee0c8b2d8348dc2a1100bda30e
Philip Lee <phil@buzzlabs.com>
2011-11-01 13:37:31 -0700

    use encode64 and gsub newlines instead of strict (only available in 1.9.2)

--- lib/citygrid/api/ad_center/image.rb
+++ lib/citygrid/api/ad_center/image.rb
@@ -6,7 +6,7 @@ class CityGrid
       class Image < AdCenter
         def self.upload user_id, name, type, image_path, options = {}
           token = extract_auth_token options
-          image_data = Base64.strict_encode64(File.open(image_path).read.to_s)
+          image_data = Base64.encode64(File.open(image_path).read.to_s).gsub(/\n/, "")
           format = options[:format] || image_path.split(".").last
           request_and_handle :post,
             "#{base_uri}/#{endpoint}/upload",
