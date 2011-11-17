83cf2e7b1e13fb2f9f20d217b5a0f2144153406c
Philip Lee <phil@buzzlabs.com>
2011-11-17 15:08:43 -0800

    return nil when no results

--- test/test_search.rb
+++ test/test_search.rb
@@ -19,3 +19,23 @@ context "searching for a CityGrid listing" do
     end.empty?
   end
 end
+
+context "searching for a special CityGrid listing" do
+  setup do
+    CityGrid.search :what => "Divine Dynasty Cleaners", :where => "84129"
+  end
+
+  should "create an array" do
+    topic.is_a? Array
+  end
+
+  should_not "be empty" do
+    topic.empty?
+  end
+
+  should "create an array containing Listing objects" do
+    topic.reject do |listing|
+      listing.is_a? CityGrid::Listing
+    end.empty?
+  end
+end
