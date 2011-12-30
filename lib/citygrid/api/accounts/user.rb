a10f5bc63168ddee1a169101edf8789f9d354762
Philip Lee <phil@buzzlabs.com>
2011-12-30 11:47:58 -0800

    fix user

--- lib/citygrid/api/accounts/user.rb
+++ lib/citygrid/api/accounts/user.rb
@@ -5,7 +5,15 @@ class CityGrid
         extend CityGrid::API::Mutatable
         extend CityGrid::API::Searchable
         
-        define_action :login, :post, "login", :auth_token => false, :format => false        
+        def self.login options = {}
+          request_and_handle :post,
+            "#{endpoint}/login",
+            :query => options
+        end
+        
+        # login behaves weird right now - expects string as body, not json encoded. 
+        # define_action :login, :post, "login", :auth_token => false, :format => false        
+        
         define_action :validate, :get, "validate", :auth_token => true, :format => false
       end
     end
--- test/api/accounts/test_account.rb
+++ test/api/accounts/test_account.rb
@@ -76,46 +76,46 @@ context "create an account" do
   email = "#{username}@a.com"
   password = "randuserpass"
   
-  # setup do
-  #   SessionHelper.sales_coord.call_api CityGrid::API::Accounts::Account,
-  #     :mutate,
-  #     "mutateOperationListResource" => [{
-  #       "operator" => "ADD",
-  #       "operand"  => {
-  #         "firstName"    => "nico-api",
-  #         "lastName"     => "gomez-api",
-  #         "phone"        => "9001111112",
-  #         "email"        => email,
-  #         "userName"     => username,
-  #         "password"     => password,
-  #         "businessName" => "businessProveApi",
-  #         "address1"     => "dir-api",
-  #         "city"         => "montevideo",
-  #         "state"        => "Montevideo",
-  #         "zipCode"      => "90069"
-  #       }
-  #     }]
-  # end
-  # should("not be empty"){ !topic.empty? }
-  # should("return message OK"){ topic.accountList.first.response.message }.equals("OK")  
-  # should("return response code OK"){ topic.accountList.first.response.code.to_i }.equals(200)
+  setup do
+    SessionHelper.sales_coord.call_api CityGrid::API::Accounts::Account,
+      :mutate,
+      "mutateOperationListResource" => [{
+        "operator" => "ADD",
+        "operand"  => {
+          "firstName"    => "nico-api",
+          "lastName"     => "gomez-api",
+          "phone"        => "9001111112",
+          "email"        => email,
+          "userName"     => username,
+          "password"     => password,
+          "businessName" => "businessProveApi",
+          "address1"     => "dir-api",
+          "city"         => "montevideo",
+          "state"        => "Montevideo",
+          "zipCode"      => "90069"
+        }
+      }]
+  end
+  should("not be empty"){ !topic.empty? }
+  should("return message OK"){ topic.accountList.first.response.message }.equals("OK")  
+  should("return response code OK"){ topic.accountList.first.response.code.to_i }.equals(200)
   
-  # context "then logging in" do
-  #   setup do
-  #     CityGrid.login :username => username, :password => password
-  #   end
-  #   should("return an authToken"){ topic.authToken }
-  # end
-  # 
-  # context "then validate with session" do
-  #   setup do
-  #     session = CityGrid.session username, password
-  #     session.call_api CityGrid::API::Accounts::User, :validate, :oauth_token => session.auth_token
-  #   end
-  #   
-  #   should("match on display_name") { topic.display_name }.equals(username)
-  #   should("match on email") { topic.email }.equals(email)
-  # end
+  context "then logging in" do
+    setup do
+      CityGrid.login :username => username, :password => password
+    end
+    should("return an authToken"){ topic.authToken }
+  end
+  
+  context "then validate with session" do
+    setup do
+      session = CityGrid.session username, password
+      session.call_api CityGrid::API::Accounts::User, :validate, :oauth_token => session.auth_token
+    end
+    
+    should("match on display_name") { topic.display_name }.equals(username)
+    should("match on email") { topic.email }.equals(email)
+  end
   
   context "then validate with raw API" do
     setup do
