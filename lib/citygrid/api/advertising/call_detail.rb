97eeaac24325468f13683959d0c9cb447d7a7e2e
chenjo <Joseph.Chen@citygridmedia.com>
2012-01-12 16:04:43 -0800

    Updating calldetail endpoint

--- lib/citygrid/api/advertising/call_detail.rb
+++ lib/citygrid/api/advertising/call_detail.rb
@@ -1,8 +1,8 @@
 class CityGrid
   class API
     class Advertising
-      class CallDetail < Advertising        
-        define_action :campaign, :post, "campaign", :auth_token => true, :format => false
+      class CallDetail < Advertising
+        define_action :search, :post, "get", :auth_token => true, :format => false
         define_action :note, :post, "note", :auth_token => true, :format => false
       end
     end
--- test/api/advertising/test_call_detail.rb
+++ test/api/advertising/test_call_detail.rb
@@ -1,10 +1,11 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'helper'))
 
 context "search for call detail" do
+  set :vcr, false
   context "by campaign" do
     setup do
       SessionHelper.sales_coord.call_api CityGrid::API::Advertising::CallDetail,
-        :campaign,
+        :search,
         :campaignId => 786,
         :period     => 'last30Days'
     end
@@ -13,7 +14,21 @@ context "search for call detail" do
       topic.callDetailsResources.length > 0
     }
   end
+  
+  context "by adgroup" do
+    setup do
+      SessionHelper.sales_coord.call_api CityGrid::API::Advertising::CallDetail,
+        :search,
+        :adGroupId  => 7264632,
+        :period     => 'last30Days'
+    end
+    should("not be empty"){ !topic.empty? }
+    should("have call detail resources"){
+      topic.callDetailsResources.length > 0
+    }
+  end
 
+  # Can't really do this test if can't reliably access the same note again and again
   # context "note" do
   #   setup do
   #     run_with_rescue do
