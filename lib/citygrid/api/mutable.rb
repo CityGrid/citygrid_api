67239564f59518dc2179226ce62488d8fab39517
Javier Evans <Javier.Evans@citygridmedia.com>
2012-12-06 13:05:10 -0800

    added ability to mutate without appending 'mutate' action name via mutate_post_only

--- lib/citygrid/api/mutable.rb
+++ lib/citygrid/api/mutable.rb
@@ -7,6 +7,9 @@ class CityGrid
         base.define_action :mutate, :post, "mutate",
           :auth_token => true,
           :format => false
+        base.define_action :mutate_post_only, :post, nil,
+          :auth_token => true,
+          :format => false
       end
     end
   end
