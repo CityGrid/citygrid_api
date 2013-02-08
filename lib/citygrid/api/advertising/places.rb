f23e9348f46ec9de095aedd1af206f0ed9d181ae
Joseph Chen <joseph@urbanspoon.com>
2013-02-08 15:10:09 -0800

    Adding place status endpoint to check whether a place has made it through the pipeline yet

--- lib/citygrid/api/advertising/places.rb
+++ lib/citygrid/api/advertising/places.rb
@@ -4,6 +4,7 @@ class CityGrid
       class Places < Advertising
         include CityGrid::API::Mutable
         include CityGrid::API::Searchable
+        define_action :status, :get, "status", :auth_token => true, :format => false
       end
     end
   end
