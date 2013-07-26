3b9584230ed9ac5bf844e54c35b4f855b569169b
Vishal Kumar <Vishal.Kumar@citygridmedia.com>
2013-07-26 14:42:25 +0530

    bug in endpoint method.. sets endpoint to nil after first use

--- lib/citygrid/api/accounts/login.rb
+++ lib/citygrid/api/accounts/login.rb
@@ -3,12 +3,12 @@ class CityGrid
     class Accounts
       class Login < Accounts
         def self.login options = {}
-          if endpoint.blank? and base_uri.blank?
-            endpoint = CityGrid::API::Accounts::User.endpoint
-            base_uri = CityGrid::API::Accounts::User.base_uri
+          if self.endpoint.blank? and self.base_uri.blank?
+            self.endpoint CityGrid::API::Accounts::User.endpoint
+            self.base_uri CityGrid::API::Accounts::User.base_uri
           end
           request_and_handle :post,
-            "#{endpoint}/login",
+            "#{self.endpoint}/login",
             :query => options,
             :headers => merge_headers()
         end
